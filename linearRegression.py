58aa483d1215d1d8d10563b1c7c28b24a9d29c2b
Jed Miguel <jedmig4@gmail.com>
2022-09-06 21:23:51 -0700

    refactored code and changed wording

--- linearRegression.py
+++ linearRegression.py
@@ -9,17 +9,14 @@ from random import uniform
 from sklearn.datasets import fetch_california_housing
 from scipy import optimize as op
 from sklearn.datasets import make_regression
-def linearRegression(df):
-    X = df.data.to_numpy()
-    y = df.target.to_numpy()
+def linearRegression(X, y):
     m, n = X.shape # m = number of examples, n = number of features
     theta_ini = np.zeros(n + 1) # theta_0 is the bias variable
     theta_ini[0] = 1 # convention is that theta_0 = 1
     X_bias = np.insert(X, 0, np.ones(X.shape[0]), axis=1) # adding column of ones for the bias variable
 
     # gradient descent
-    # method 'BFGS' returns hypothesis function close to sklearn's hypothesis
-    # method ‘Newton-CG’
+    # method 'BFGS' returns parameters closest to sklearn's
     theta = op.minimize(costFunction, theta_ini, (X_bias,y), method='BFGS').x
     return theta
 
@@ -29,15 +26,15 @@ def costFunction(theta, X, y, lambda_=0.0):
 
 def main():
     df = fetch_california_housing(as_frame=True)
-    theta = linearRegression(df)
-    skTheta = LinearRegression().fit(df.data.to_numpy(), df.target.to_numpy())
-    skTheta = np.insert(skTheta.coef_, 0, skTheta.intercept_)
     X = df.data.to_numpy()
     y = df.target.to_numpy()
+    theta = linearRegression(X, y)
+    skTheta = LinearRegression().fit(df.data.to_numpy(), df.target.to_numpy())
+    skTheta = np.insert(skTheta.coef_, 0, skTheta.intercept_)
     X_bias = np.insert(X, 0, np.ones(X.shape[0]), axis=1)  # adding column of ones for the bias variable
-    print('My implementation\'s hypothesis function:', theta)
+    print('My implementation\'s parameters:', theta)
     print('MSE of my implementation: ', mean_squared_error(y, np.matmul(X_bias, skTheta)))
-    print('skLearn\'s implementation\'s hypothesis function:',skTheta)
+    print('skLearn\'s implementation\'s parameters:',skTheta)
     print('MSE of sklearn\'s implimentation: ', mean_squared_error(y, np.matmul(X_bias, skTheta)))
 
 if __name__ == "__main__":
